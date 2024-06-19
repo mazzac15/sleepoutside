a1add58ec8f533432f6530d91bc845bb3c44dd52
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2024-06-19 22:38:56 +0100

    W02 Work Team. Fixed. removed unnecesary console.log

--- src/js/product.js
+++ src/js/product.js
@@ -3,7 +3,6 @@ import ProductDetails from "./ProductDetails.mjs";
 import { getParams } from "./utils.mjs";
 
 const productId = getParams("product");
-console.log("produt", productId);
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
