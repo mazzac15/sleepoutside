aa05a5f9abd12561ca21259473118220319b0f8c
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2024-06-19 22:37:17 +0100

    W02 Work Team. Fixed

--- src/js/product.js
+++ src/js/product.js
@@ -3,6 +3,7 @@ import ProductDetails from "./ProductDetails.mjs";
 import { getParams } from "./utils.mjs";
 
 const productId = getParams("product");
+console.log("produt", productId);
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
