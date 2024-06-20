a02e60bb96ef5dbb010700060d79d771edfdcceb
Cindy Mazza <maz22004@byui.edu>
2024-06-19 22:26:19 -0400

    delete console.log line

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -30,7 +30,7 @@ export default class ProductDetails {
   async init() {
     // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
-    console.log("product:", this.product)
+
     // once we have the product details we can render out the HTML
    this.renderProductDetails("main");
     // once the HTML is rendered we can add a listener to Add to Cart button
