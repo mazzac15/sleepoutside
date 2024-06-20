5fd5e83d78838edf180c7353164feccfc0347938
Cindy Mazza <maz22004@byui.edu>
2024-06-20 13:52:06 -0400

    script line html add

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -28,17 +28,23 @@ export default class ProductDetails {
     this.dataSource = dataSource;
   }
   async init() {
-    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-    this.product = await this.dataSource.findProductById(this.productId);
-
-    // once we have the product details we can render out the HTML
-   this.renderProductDetails("main");
-    // once the HTML is rendered we can add a listener to Add to Cart button
-    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-    document
-      .getElementById("addToCart")
-      .addEventListener("click", this.addToCart.bind(this));
-  }
+    try {
+      // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+      this.product = await this.dataSource.findProductById(this.productId);
+      if (!this.product) {
+        throw new Error(`Product with ID ${this.productId} not found`);
+      }
+      // once we have the product details we can render out the HTML
+      this.renderProductDetails("main");
+      // once the HTML is rendered we can add a listener to Add to Cart button
+      // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+      document
+        .getElementById("addToCart")
+        .addEventListener("click", this.addToCart.bind(this));
+    } catch (error) {
+      return ("Error intializing ProduckDetails:", error);
+    }
+  }  
   addToCart() {
     setLocalStorage("so-cart", this.product);
   }
