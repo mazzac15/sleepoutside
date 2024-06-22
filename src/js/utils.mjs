032a54fe4118532dfdf93404f18a07194593e37a
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2024-06-22 13:16:04 +0100

    W02 Individual Activity: Dynamic Product List

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,3 +28,18 @@ export function getParams(param) {
   const product = urlParams.get(param)
   return product;
 }
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
