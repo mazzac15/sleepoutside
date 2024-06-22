31cd4688cfa256bbd3c740bd7b439a03fe7e344c
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2024-06-22 21:18:39 +0100

    W02 Project: Report on Tasks - Remove from cart feature

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,5 @@ import ProductList from "./ProductList.mjs";
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
-console.log("listing: ", listing);
 
 listing.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -10,8 +10,49 @@ export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
-export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+export function setLocalStorage(key, product) {
+  //get the new data
+  const existingData = localStorage.getItem(key);
+  // create a list to store the products
+  let updatedData = [];
+  // if any, covert it in a json
+  if (existingData) {
+    
+    updatedData = JSON.parse(existingData);
+
+    if (!Array.isArray(updatedData)) {
+      updatedData = [];
+    }
+  }
+  // Append the new product to the existing product list
+  updatedData.push(product);
+
+  // Store the updated data back to localStorage
+  localStorage.setItem(key, JSON.stringify(updatedData));
+}
+
+export function deleteLocalStorage(key, productId) {
+   
+  const existingData = localStorage.getItem(key);
+
+  let updatedData = [];
+
+  if (existingData) {
+    updatedData = JSON.parse(existingData);
+
+    if (!Array.isArray(updatedData)) {
+      updatedData = [];
+    }
+  }
+  // THis will find the index of rthe first item. It return -1 if no item found
+  const index = updatedData.findIndex(item => item.Id === productId);
+
+  // If found, remove that item from the array with splice
+  if (index !== -1) {
+    updatedData.splice(index, 1);
+  }
+
+  localStorage.setItem(key, JSON.stringify(updatedData));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
