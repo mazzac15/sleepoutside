4bc578f133ebf1b109747e1a8dbb694765a67426
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2024-06-30 00:16:43 +0100

    Total$ in Cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import { getMultipleParams, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-// Uisng new getMultipleParams to get both category and product from the url e.g.: /tent/RT53F. Reason for this is because in previous version, 
+// Uisng new getMultipleParams to get both category and product from the url e.g.: /tent/RT53F. Reason for this is because in previous version,
 //the category was missing, so I thought to pass the category via the href. You'll see the modification also in the href in productList, in the template.
 //
 const { category, product } = getMultipleParams("category", "product");
