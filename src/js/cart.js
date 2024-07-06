0d48571c8d3145e8b1aad8a661ce1b9624a8aee5
tborjal <bor22014@byui.edu>
2024-07-06 21:40:35 +0800

    configuring issues

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { loadHeaderFooter, updateCartBadge } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
